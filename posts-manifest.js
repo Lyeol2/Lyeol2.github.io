c6ed176dff611edcaf24d9b4218c2fa64b86934f
Lyeol2 <frog100f@gmail.com>
2026-05-23 05:38:16 +0900

    포스트 추가

--- posts-manifest.js
+++ posts-manifest.js
@@ -1,42 +1,58 @@
 window.BLOG_POSTS = [
   {
-    path: "posts/Blog/devlog/Github Blog Dev - 1.md",
-    category: "Blog / devlog",
-    display: "블로그 개발 일지 #1",
-    description: "깃허브 페이지 사용 방법",
-    recent: "2026-05-22",
-    views: 12,
-    tags: ["devlog"],
-    image: "posts/_assets/Github Blog Dev - 1/Github Blog Dev - 1_image.png"
+    "path": "posts/Algorithm/Boids.md",
+    "category": "Algorithm",
+    "display": "Boids",
+    "description": "",
+    "recent": "2026-05-22",
+    "views": 3,
+    "tags": [
+      "algorithm"
+    ],
+    "image": ""
   },
   {
-    path: "posts/Blog/devlog/Github Blog Dev - 2.md",
-    category: "Blog / devlog",
-    display: "블로그 개발 일지 #2",
-    description: "html 과 css 꾸미기",
-    recent: "2026-05-22",
-    views: 8,
-    tags: ["devlog"],
-    image: "posts/_assets/Github Blog Dev - 2/Github Blog Dev - 2_gif.gif"
+    "path": "posts/Blog/devlog/Github Blog Dev - 1.md",
+    "category": "Blog / devlog",
+    "display": "블로그 개발 일지 #1",
+    "description": "깃허브 페이지 사용 방법",
+    "recent": "2026-05-22",
+    "views": 12,
+    "tags": [
+      "devlog"
+    ],
+    "image": "posts/_assets/Github Blog Dev - 1/Github Blog Dev - 1_image.png"
   },
   {
-    path: "posts/Blog/obsidian/옵시디언 플러그인 추천.md",
-    category: "Blog / obsidian",
-    display: "옵시디언 플러그인 추천",
-    description: "내가 쓰던 플러그인 모아 놓기",
-    recent: "2026-05-22",
-    views: 5,
-    tags: [],
-    image: "posts/_assets/옵시디언 플러그인 추천/옵시디언 플러그인 추천_image.png"
+    "path": "posts/Blog/devlog/Github Blog Dev - 2.md",
+    "category": "Blog / devlog",
+    "display": "블로그 개발 일지 #2",
+    "description": "html 과 css 꾸미기",
+    "recent": "2026-05-22",
+    "views": 8,
+    "tags": [
+      "devlog"
+    ],
+    "image": "posts/_assets/Github Blog Dev - 2/Github Blog Dev - 2_gif.gif"
   },
   {
-    path: "posts/Algorithm/Boids.md",
-    category: "Algorithm",
-    display: "Boids",
-    description: "",
-    recent: "2026-05-22",
-    views: 3,
-    tags: ["algorithm"],
-    image: ""
+    "path": "posts/Blog/devlog/Github Blog Dev - 3.md",
+    "category": "Blog / devlog",
+    "display": "블로그 개발 일지 #3",
+    "description": "post 기능 만들기",
+    "recent": "2026-05-22",
+    "views": 0,
+    "tags": [],
+    "image": "posts/_assets/Github Blog Dev - 3/Github Blog Dev - 3.gif.gif"
+  },
+  {
+    "path": "posts/Blog/obsidian/옵시디언 플러그인 추천.md",
+    "category": "Blog / obsidian",
+    "display": "옵시디언 플러그인 추천",
+    "description": "내가 쓰던 플러그인 모아 놓기",
+    "recent": "2026-05-22",
+    "views": 5,
+    "tags": [],
+    "image": "posts/_assets/옵시디언 플러그인 추천/옵시디언 플러그인 추천_image.png"
   }
 ];

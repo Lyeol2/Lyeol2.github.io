1eafefe9fe053174da9395a81f327b3d71cdb2ca
Lyeol2 <frog100f@gmail.com>
2026-06-08 08:49:47 +0900

    Localize vlog post images

--- posts-manifest.js
+++ posts-manifest.js
@@ -9,7 +9,7 @@ window.BLOG_POSTS = [
     "tags": [
       "vlog"
     ],
-    "image": ""
+    "image": "posts/_assets/올림픽공원시위 후기/olympic-protest-01.webp"
   },
   {
     "path": "posts/Project/AI Project/AI Project - 5.md",
@@ -224,16 +224,6 @@ window.BLOG_POSTS = [
     "image": "posts/_assets/옵시디언 플러그인 추천/옵시디언 플러그인 추천_image.png"
   },
   {
-    "path": "posts/Algorithm/무제.md",
-    "category": "Algorithm",
-    "display": "무제",
-    "description": "",
-    "recent": "",
-    "views": 0,
-    "tags": [],
-    "image": ""
-  },
-  {
     "path": "posts/개발 메모.md",
     "category": "",
     "display": "개발 메모",
@@ -253,6 +243,16 @@ window.BLOG_POSTS = [
     "tags": [
       "[excalidraw]"
     ],
+    "image": ""
+  },
+  {
+    "path": "posts/Algorithm/무제.md",
+    "category": "Algorithm",
+    "display": "무제",
+    "description": "",
+    "recent": "",
+    "views": 0,
+    "tags": [],
     "image": ""
   }
 ];
